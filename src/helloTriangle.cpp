0400c7622ac1e55143dae7aa1532df0b3eaa6f8f
gommness <dev.gomezjavier@gmail.com>
2020-06-22 21:36:00 +0200

    rendering a triangle

--- src/helloTriangle.cpp
+++ src/helloTriangle.cpp
@@ -4,13 +4,78 @@
 
 #include <iostream>
 
+#define STR(X) #X
+
+#define VERTEX_SRC "\
+        #version 330 core\n\
+        layout (location = 0) in vec3 position;\n\
+        void main(){\n\
+            gl_Position = vec4(position.x, position.y, position.z, 1.0);\n\
+        }"
+#define FRAGMENT_SRC "\
+        #version 330 core\n\
+        out vec4 color;\n\
+        void main(){\n\
+            color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n\
+        }"
+
+
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode){
     if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GL_TRUE);
 }
 
+GLint compileShader(GLuint & shader, GLuint type, const GLchar** src, const char* shaderName){
+    GLint success;
+    GLchar infoLog[512];
+
+    // creamos el objeto shader
+    shader = glCreateShader(type);
+    // cargamos el codigo del shader
+    glShaderSource(shader, 1, src, NULL);
+    // compilamos el shader
+    glCompileShader(shader);
+    // comprobamos si la compilacion del shader ha sido correcta o no
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if(!success){
+        glGetShaderInfoLog(shader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER" << shaderName << "COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    return success;
+}
+
+GLint linkShaders(GLuint & shaderProgram, GLuint & vertexShader, GLuint & fragmentShader){
+    GLint success;
+    GLchar infoLog[512];
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if(!success){
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINK_FAILED\n" << infoLog << std::endl;
+    }
+}
+
 int main(){
+
+    GLfloat vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f
+    };
+    GLuint VBO;
+    GLuint VAO;
+    GLuint vertexShader;
+    GLuint fragmentShader;
+    GLuint shaderProgram;
+    GLint success;
+    GLchar infoLog[512];
+    const GLchar* vertexShaderSource = VERTEX_SRC;
+    const GLchar* fragmentShaderSource = FRAGMENT_SRC;
+
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -32,6 +97,38 @@ int main(){
         return -1;
     }
 
+    glGenVertexArrays(1, &VAO);
+    // obtenemos un id unico para el buffer de vertices
+    glGenBuffers(1, &VBO);
+
+    compileShader(vertexShader, GL_VERTEX_SHADER, &vertexShaderSource, "VERTEX");
+    compileShader(fragmentShader, GL_FRAGMENT_SHADER, &fragmentShaderSource, "FRAGMENT");
+    linkShaders(shaderProgram, vertexShader, fragmentShader);
+
+    // ya no necesitamos los shaders, asi que los liberamos
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // enlazamos el vertex array object
+    glBindVertexArray(VAO);
+    // bindeamos el buffer de OpenGL al que hemos creado
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    // copiamos nuestros vertices al buffer
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    // hemos de indicarle a OpenGL que input vamos a determinar (location = 0)
+    // la dimension del atributo del vertice (3 porque pasamos vertices en 3D)
+    // el tipo de datos de los vertices
+    // si OpenGL deberia normalizar las coordenadas o no
+    // y el comienzo del vector de datos
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(GLfloat), (GLvoid*)0);
+    // NOTA: los atributos de vertices toman los datos del VBO que esta actualmente enlazado al GL_ARRAY_BUFFER
+    // a la hora de llamar a glVertexAttribPointer. de esta forma, hemos dicho que el vertice (entrada del shader)
+    // de localizacion 0 (en el shader, location = 0) tomara los datos del actual VBO.
+    glEnableVertexAttribArray(0);
+    // des-enlazamos el VAO
+    glBindVertexArray(0);
+
+
     glViewport(0, 0, 800, 600);
 
 
@@ -40,6 +137,11 @@ int main(){
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
 
         glfwSwapBuffers(window);
     }
